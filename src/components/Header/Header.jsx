e2a8af01b5697af5b5ea9cbdb0f6865bacef93d6
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-02-11 20:53:57 +0330

    some code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,9 @@ import { MdLocationOn } from "react-icons/md";
 import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi";
 import { useState, useRef } from "react";
 import useOutsideClick from "../../hooks/useOutsideClick";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRange } from "react-date-range";
 
 function Header() {
   const [destination, setDestination] = useState("");
