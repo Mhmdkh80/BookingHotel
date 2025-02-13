87c55dc84667a791aa5a8a6e173d226f64171e0a
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-02-13 18:50:54 +0330

    searchParams

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,10 @@ import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRange } from "react-date-range";
 import { format } from "date-fns";
+import {
+  createSearchParams,
+  useNavigate,
+} from "react-router-dom";
 
 function Header() {
   const [destination, setDestination] = useState("");
@@ -23,12 +27,28 @@ function Header() {
     },
   ]);
   const [openDate, setOpenDate] = useState(false);
+  const navigate = useNavigate();
+  // const [searchParams, setSearchParams] = useSearchParams;
+
   const handleOptions = (name, operation) => {
     setOptions((prev) => {
       return {
         ...prev,
         [name]: operation === "inc" ? options[name] + 1 : options[name] - 1,
       };
+    });
+  };
+
+  const handleSearch = () => {
+    const encodedParams = createSearchParams({
+      date: JSON.stringify(date),
+      destination,
+      options: JSON.stringify(options),
+    });
+    // setSearchParams(encodedParams);
+    navigate({
+      pathname: "/hotels",
+      search: encodedParams.toString(),
     });
   };
 
@@ -84,7 +104,7 @@ function Header() {
           <span className="separator"></span>
         </div>
         <div className="headerSearchItem">
-          <button className="headerSearchBtn">
+          <button className="headerSearchBtn" onClick={handleSearch}>
             <HiSearch className="headerICon" />
           </button>
         </div>
