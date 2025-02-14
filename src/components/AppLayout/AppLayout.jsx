c0bf5988b71b8f0e398b0bdfbd5f73e37ade2a38
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-02-14 21:12:37 +0330

    nested route

---
+++ src/components/AppLayout/AppLayout.jsx
@@ -0,0 +1,10 @@
+function AppLayout() {
+  return (
+    <div className="appLayout">
+      <div className="sidebar">sidebar</div>
+      <div className="mapContainer">map</div>
+    </div>
+  );
+}
+
+export default AppLayout;
