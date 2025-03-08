8c666b1b72843cbe1d571200505178baddbb39e4
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-03-08 18:29:39 +0330

    complete newbookmark

--- src/components/AddNewBookmark/AddNewBookmark.jsx
+++ src/components/AddNewBookmark/AddNewBookmark.jsx
@@ -1,5 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import useUrlLocation from "../../hooks/useUrlLocation";
+
 function AddNewBookmark() {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [lat, lng] = useUrlLocation();
+
+  return (
+    <div>
+      <h2>Bookmark new location</h2>
+      <form className="form">
+        <div className="formControl">
+          <label htmlFor="CityName">CityName</label>
+          <input type="text" name="CityName" id="CityName" />
+          <label htmlFor="Country">Country</label>
+          <input type="text" name="Country" id="Country" />
+        </div>
+        <div className="buttons">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(-1);
+            }}
+            className="btn btn--back"
+          >
+            {" "}
+            &larr; Back
+          </button>
+          <button className="btn btn--primary"> Add</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default AddNewBookmark;
