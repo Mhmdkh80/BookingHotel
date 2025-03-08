8d84c48f883243695f27becd5dd0ebb303606300
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-03-08 17:22:40 +0330

    change css

--- src/components/SingleBookmark/SingleBookmark.jsx
+++ src/components/SingleBookmark/SingleBookmark.jsx
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useBookmark } from "../Context/BookmarkListContext";
 import { useEffect } from "react";
 import Loader from "../Loader/Loader";
+import ReactCountryFlag from "react-country-flag";
 
 function SingleBookmark() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { getBookmark, isLoadingCurrBookmark, currentBookmark } = useBookmark();
   useEffect(() => {
@@ -11,7 +13,19 @@ function SingleBookmark() {
   }, [id]);
 
   if (isLoadingCurrBookmark || !currentBookmark) return <Loader />;
-  return <div>SingleBookmark</div>;
+  return (
+    <div>
+      <button onClick={() => navigate(-1)} className="btn btn--back">
+        &larr; Back
+      </button>
+      <h2 className="bookmarkHeader">{currentBookmark.cityName}</h2>
+      <div className="bookmarkItem">
+        <ReactCountryFlag svg countryCode={currentBookmark.countryCode} />
+        &nbsp; <strong>{currentBookmark.cityName}</strong> &nbsp;
+        <span>{currentBookmark.country}</span>
+      </div>
+    </div>
+  );
 }
 
 export default SingleBookmark;
