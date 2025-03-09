9a32de487d795414e520e9b7957f2ac7662f4347
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-03-09 21:20:15 +0330

    fetchLocationData

--- src/components/AddNewBookmark/AddNewBookmark.jsx
+++ src/components/AddNewBookmark/AddNewBookmark.jsx
@@ -1,12 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import useUrlLocation from "../../hooks/useUrlLocation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const BASE_GEOCODING_URL =
+  "https://us1.api-bdc.net/data/reverse-geocode-client";
 
 function AddNewBookmark() {
   const navigate = useNavigate();
   const [lat, lng] = useUrlLocation();
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
+
+  useEffect(() => {
+    async function fetchLocationData() {
+      try {
+        const { data } = await axios.get(
+          `${BASE_GEOCODING_URL}?latitude=${lat}&longitude=${lng}`
+        );
+        setCityName(data.city || data.locality || "");
+        setCountry(data.countryName);
+      } catch (error) {}
+    }
+    fetchLocationData();
+  }, [lat, lng]);
 
   return (
     <div>
