8dface03f3c46464b1ca79e3689fa086dae93662
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-04-13 22:19:43 +0330

    authReducer

--- src/components/Context/authProvider.jsx
+++ src/components/Context/authProvider.jsx
@@ -1,3 +1,22 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const authContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+function authReducer(state, action) {}
+
+export default function AuthContextProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    authReducer,
+    initialState
+  );
+
+  function login(){}
+
+
+  function logout(){}
+  return <authContext.Provider>{children}</authContext.Provider>;
+}
