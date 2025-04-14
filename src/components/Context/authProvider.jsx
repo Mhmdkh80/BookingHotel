b8a3316a74d6d7d4773182b82dd69f35a11a203d
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-04-14 22:00:56 +0330

    login and logout

--- src/components/Context/authProvider.jsx
+++ src/components/Context/authProvider.jsx
@@ -6,7 +6,16 @@ const initialState = {
   user: null,
   isAuthenticated: false,
 };
-function authReducer(state, action) {}
+
+function authReducer(state, action) {
+  switch(action.type)
+}
+
+const Fake_User = {
+  name: "mohamad",
+  email: "user@gmail.com",
+  password: "1234",
+};
 
 export default function AuthContextProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
@@ -14,9 +23,15 @@ export default function AuthContextProvider({ children }) {
     initialState
   );
 
-  function login(){}
+  function login(email, password) {
+    if (email === Fake_User.email && password === Fake_User.password)
+      dispatch({ type: "login", payload: Fake_User });
+  }
 
-
-  function logout(){}
-  return <authContext.Provider>{children}</authContext.Provider>;
+  function logout() {}
+  return (
+    <authContext.Provider value={(user, isAuthenticated, login, logout)}>
+      {children}
+    </authContext.Provider>
+  );
 }
