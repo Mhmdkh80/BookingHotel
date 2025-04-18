56dcf3ac720fe31a24da44de561542e41dadeb1f
mhmdkh80 <mohamadkh.tech@gmail.com>
2025-04-18 22:48:52 +0330

    navigate protect

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import { useEffect } from "react";
 
-function ProtectedRoute() {
-  return <div></div>;
+function ProtectedRoute({ children }) {
+    const {isAuthenticated} = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/");
+  }, [isAuthenticated, navigate]);
+  return isAuthenticated ? children : null;
 }
 
 export default ProtectedRoute;
